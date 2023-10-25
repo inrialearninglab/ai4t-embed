ee1dfefdbda15b433b98188fa9ef6d51a016625f
Benoit Rospars <benoit.rospars@inria.fr>
2023-10-25 16:29:09 +0200

    Setup embed configuration

--- docs/assets/extra.js
+++ docs/assets/extra.js
@@ -11,6 +11,28 @@ window.MathJax = {
   }
 };
 
+function copyLink() {
+  const input = document.getElementById('inputlink');
+  const copy = document.getElementById('copylink');
+  input.value = location.href;
+  copy.addEventListener('click', () => {
+    input.select();
+    const url = input.value;
+    navigator.clipboard.writeText(url);
+    window.alert('Link copied to the clipboard:\n'+url);
+  })
+}
+copyLink();
+
+function iframeResize() {
+  var heightSidebar = document.querySelector('.md-sidebar__scrollwrap').offsetHeight;
+  var heightContent = document.querySelector('.md-content__inner').offsetHeight;
+  var height = Math.max(heightSidebar, heightContent) + 100 + 50 + 116;
+  parent.postMessage("resize::"+height,"*");
+}
+iframeResize();
+setInterval(iframeResize, 300);
+
 var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['trackPageView']);
