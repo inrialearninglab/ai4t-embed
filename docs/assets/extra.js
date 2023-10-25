21138fb0904b116ad279194cbf2a61cf9aba0652
Benoit Rospars <benoit.rospars@inria.fr>
2023-10-25 14:43:58 +0200

    Redesign lang select and add copy link button

--- docs/assets/extra.js
+++ docs/assets/extra.js
@@ -11,6 +11,19 @@ window.MathJax = {
   }
 };
 
+function copyLink() {
+  const input = document.getElementById('inputlink');
+  const copy = document.getElementById('copylink');
+  input.value = location.href;
+  copy.addEventListener('click', () => {
+    input.select();
+    const url = input.value;
+    navigator.clipboard.writeText(url);
+    window.alert('Link copied to the clipboard:\n'+url);
+  })
+}
+copyLink();
+
 function iframeResize() {
   var heightSidebar = document.querySelector('.md-sidebar__scrollwrap').offsetHeight;
   var heightContent = document.querySelector('.md-content__inner').offsetHeight;
@@ -18,7 +31,7 @@ function iframeResize() {
   parent.postMessage("resize::"+height,"*");
 }
 iframeResize();
-setInterval(iframeResize, 1000);
+setInterval(iframeResize, 300);
 
 var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
